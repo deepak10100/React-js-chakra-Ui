8166923107c52e0d5722d0ed8fcec309e75946f7
deepak <deepakmalakar98185@gmail.com>
2023-12-23 00:28:47 +0530

    23 day

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -1,11 +1,19 @@
-import { Heading } from '@chakra-ui/react'
+import { Heading,VStack,Box,Center, StackDivider, Input } from '@chakra-ui/react'
 import React from 'react'
-
+import { AiOutlineCloudUpload } from "react-icons/ai";
 function Upload() {
   return (
-    <div>
-      <Heading>Uploads</Heading>
-    </div>
+    <>
+    
+
+<Center  h='100vh'>
+<VStack>
+<AiOutlineCloudUpload size={'50%'} />
+<Input type='file'/>
+</VStack>
+</Center>
+     
+    </>
   )
 }
 
